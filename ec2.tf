8ee8c34c1dd0523ef01979c1d94bf4404e8ef279
DanielMelo1 <dreimao4@gmail.com>
2025-02-23 23:39:16 -0300

    Atualizado ec2.tf com ELB e ajustes

--- ec2.tf
+++ ec2.tf
@@ -10,26 +10,26 @@ resource "aws_instance" "web_1" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = "key"
 
-  # Script de inicialização (User Data) para configurar a EC2 automaticamente
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install -y git nodejs npm postgresql15
 
-    # Clonar o repositório da aplicação
+    # Clonar o repositório correto
     cd /home/ec2-user
-    git clone https://github.com/DanielMelo1/e-commerce-app.git
-    cd ecommerce-app/backend
+    git clone https://github.com/DanielMelo1/e-commerce-application.git
+    cd e-commerce-application/backend
 
-    # Instalar dependências e iniciar backend
+    # Iniciar backend
     npm install
     npm run dev &
-    
+
+    # Iniciar frontend
     cd ../frontend
     npm install
     npm run build
     npm install -g serve
-    serve -s dist -l 5173 &
+    serve -s dist -l 80 &
   EOF
 
   tags = {
@@ -45,26 +45,26 @@ resource "aws_instance" "web_2" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = "key"
 
-  # Script de inicialização (User Data) para configurar a EC2 automaticamente
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install -y git nodejs npm postgresql15
 
-    # Clonar o repositório da aplicação
+    # Clonar o repositório correto
     cd /home/ec2-user
-    git clone https://github.com/seu-usuario/ecommerce-app.git
-    cd ecommerce-app/backend
+    git clone https://github.com/DanielMelo1/e-commerce-application.git
+    cd e-commerce-application/backend
 
-    # Instalar dependências e iniciar backend
+    # Iniciar backend
     npm install
     npm run dev &
-    
+
+    # Iniciar frontend
     cd ../frontend
     npm install
     npm run build
     npm install -g serve
-    serve -s dist -l 5173 &
+    serve -s dist -l 80 &
   EOF
 
   tags = {
@@ -75,7 +75,7 @@ resource "aws_instance" "web_2" {
 # Criar Volume EBS para Web 1
 resource "aws_ebs_volume" "web1_ebs" {
   availability_zone = "us-east-1a"
-  size             = 10  # Tamanho em GB
+  size             = 10
 
   tags = {
     Name = "empresa-x-web-1-ebs"
@@ -92,7 +92,7 @@ resource "aws_volume_attachment" "web1_attach" {
 # Criar Volume EBS para Web 2
 resource "aws_ebs_volume" "web2_ebs" {
   availability_zone = "us-east-1b"
-  size             = 10  # Tamanho em GB
+  size             = 10
 
   tags = {
     Name = "empresa-x-web-2-ebs"
@@ -104,4 +104,96 @@ resource "aws_volume_attachment" "web2_attach" {
   device_name = "/dev/xvdg"
   volume_id   = aws_ebs_volume.web2_ebs.id
   instance_id = aws_instance.web_2.id
+}
+
+# Criar Security Group para o Load Balancer
+resource "aws_security_group" "elb_sg" {
+  name        = "empresa-x-elb-sg"
+  description = "Permitir tráfego para o Load Balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "empresa-x-elb-sg"
+  }
+}
+
+# Criar Load Balancer
+resource "aws_lb" "app_lb" {
+  name               = "empresa-x-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_sg.id]
+  subnets           = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "empresa-x-lb"
+  }
+}
+
+# Criar Target Group para associar as instâncias EC2 ao Load Balancer
+resource "aws_lb_target_group" "app_tg" {
+  name     = "empresa-x-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "empresa-x-target-group"
+  }
+}
+
+# Registrar instâncias EC2 no Target Group
+resource "aws_lb_target_group_attachment" "web1_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web2_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.web_2.id
+  port             = 80
+}
+
+# Criar Listener do Load Balancer para redirecionar tráfego para as instâncias
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
 }
